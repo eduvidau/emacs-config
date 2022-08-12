675158498c8c8cd87ca8bbd8c4a9eb5e1ce6f0fb
eduvidau <eduvidau@indiana.edu>
2022-08-12 18:57:47 -0400

    Created custum theme and load it upon starting.

--- init.el
+++ init.el
@@ -14,6 +14,11 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+;;Theme
+(setq custom-theme-directory "~/.emacs.d/themes")
+(load-theme 'lalo 1)
+
+
 (setq straight-use-package-by-default t)
 
 (straight-use-package 'use-package)
@@ -35,3 +40,16 @@
 
 ;; Start Server
 (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("447f0fe00896b127d686d7e1936301e965c160e72ffb24279056d4d03adeb91a" "77a6c33dd3377c5384eab42f000beae2bf898f44dfdf6a5d52505f929f5e2f52" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
