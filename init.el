148493e5793fce3bc685dbbfbe914806d1705059
Lalo Vidaurri <evidaurdz@gmail.com>
2022-11-02 21:33:39 -0600

    Set up js2-mode-hook to start eglot and set up global company-mode

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Basic config stuff
 
-(setq inibit-startup-screen t) ;; Inhibit Startup Screen
+(setq inhibit-startup-screen t) ;; Inhibit Startup Screen
 
 (menu-bar-mode -1) ;; Disable Menu bar
 
@@ -10,10 +10,10 @@
 (global-display-line-numbers-mode t) ;;Display line numbers
 
 (dolist (mode '(org-mode-hook ;;Don't display line numbers
-		 term-mode-hook
-		 shell-mode-hook
-		 eshell-mode-hook))
-	 (add-hook mode (lambda () (display-line-number-mode 0))))
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-number-mode 0))))
 
 ;; Elpaca Package Manager
 
@@ -76,38 +76,28 @@
 
 (elpaca-use-package magit)
 
-;; Company-mode
+;; Eglot : LSP
 
-(elpaca-use-package company)
+(elpaca-use-package eglot
+  :commands eglot
+  :hook (js2-mode . eglot-ensure))
 
+;; Company-mode : Completions
+
+(elpaca-use-package company
+  :init
+  (global-company-mode))
+
+;; ;; PL modes
 ;; Elm
 
 (elpaca-use-package elm-mode)
 
-;; Eglot
+;; JS
 
-(elpaca-use-package eglot)
-
-;; Modes based on file ending
-
-
+(elpaca-use-package js2-mode
+  :mode "\\.js\\'")
 
 ;; Start Server
 
 (server-start)
-
-;; Not sure what this is
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(custom-safe-themes
-;;    '("002ed973bd54d22d01f4d4213e88d181af2c439d8538dc856d3794f0c32f3ae1" "447f0fe00896b127d686d7e1936301e965c160e72ffb24279056d4d03adeb91a" "77a6c33dd3377c5384eab42f000beae2bf898f44dfdf6a5d52505f929f5e2f52" default)))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
